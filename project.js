889283440f115c575b687a15553b175471e92d3b
cycankahuna <supreetvats55@gmail.com>
2023-08-15 19:26:14 +0530

    added the feature  to bet on number of lines

--- project.js
+++ project.js
@@ -22,8 +22,21 @@ const deposit = () => {
 }
 }
 
+const getNumberOfLines = () => {
+    while (true){
+    const lines = propmt("Enter the number of lines to bet on (1-3): ") 
+    const numberOfLines = parseFloat(lines);
 
+    if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3){
+        console.log("Invalid number of lines, try again");
+
+    } else{
+        return numberOfLines;
+    }
+}
+}
 
 
 const despositAmount = deposit();
+const numberOfLines = getNumberOfLines();
 
